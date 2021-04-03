7127fde965f2848dcd82437edd76900ff2174f13
LonJonn <leon.salsiccia@gmail.com>
2021-04-03 12:40:20 +1100

    Clean up project card

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -1,6 +1,5 @@
-import "twin.macro";
+import tw, { styled } from "twin.macro";
 import Link from "next/link";
-import tw, { css, styled } from "twin.macro";
 import type { ProjectMetadata } from "../../types";
 
 const ProjectCard: React.FC<ProjectMetadata & { slug: string }> = ({
@@ -10,59 +9,38 @@ const ProjectCard: React.FC<ProjectMetadata & { slug: string }> = ({
   description,
   cover_image,
 }) => (
-  // Wrap in div to avoid clashing margins
   <Link href={`/project/${slug}`} passHref>
-    <a className="group" tw="lg:w-5/12">
-      <Wrapper>
-        <div tw="lg:(aspect-w-16 aspect-h-9)">
-          <img
-            src={`/images/${slug}/${cover_image}`}
-            alt={`${slug} cover image`}
-            css={imageStyles()}
-          />
+    <a className="group" tw="relative flex mb-10 lg:(w-5/12 max-h-72)">
+      <Image
+        src={`/images/${slug}/${cover_image}`}
+        alt={`${slug} cover image`}
+      />
+
+      <Info>
+        <div tw="flex items-center justify-between font-bold">
+          <h2 tw="text-2xl">{title}</h2>
+          <h3 tw="text-lg text-blue-600">{year}</h3>
         </div>
 
-        {/* Info */}
-        <Info>
-          <InfoTop>
-            <Title>{title}</Title>
-            <Year>{year}</Year>
-          </InfoTop>
-
-          <Description>{description}</Description>
-        </Info>
-      </Wrapper>
+        <p tw="text-gray-600">{description}</p>
+      </Info>
     </a>
   </Link>
 );
 
 //#region Internal Styles
 
-const Wrapper = tw.div`relative flex flex-col mb-10`;
-
-const imageStyles = () => [
-  tw`rounded-2xl shadow object-cover`,
-  tw`transform translate-y-0.5 transition-all duration-500`,
-  // States
-  tw`group-hover:(shadow-xl -translate-y-0.5)`,
-];
+const Image = styled.img(() => [
+  tw`w-full h-full rounded-2xl shadow object-cover`,
+  tw`transform transition-all duration-500`,
+  tw`group-hover:(shadow-xl -translate-y-1)`,
+]);
 
 const Info = styled.div(() => [
   tw`w-10/12 p-6 bg-whiteAlpha-600 rounded-md shadow-xl`,
   tw`absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/3`,
-  // Custom
-  css`
-    backdrop-filter: blur(20px);
-  `,
+  { backdropFilter: "blur(20px)" },
 ]);
-
-const InfoTop = tw.div`flex items-center`;
-
-const Title = tw.h2`mr-auto text-2xl font-bold`;
-
-const Year = tw.h3`text-blue-600 text-lg font-bold`;
-
-const Description = tw.p`text-gray-600`;
 
 //#endregion
 
