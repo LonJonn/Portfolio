42d2b6ca13084184b64300d916ce4621985ba20c
LonJonn <leon.salsiccia@gmail.com>
2021-04-28 16:03:23 +1000

    refactor: swap slickjs 🤒 for swiperjs 😍

--- src/components/project/ProjectShowcase.tsx
+++ src/components/project/ProjectShowcase.tsx
@@ -1,43 +1,34 @@
-import "twin.macro";
-import Slider, { Settings } from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import type { CSSProp } from "styled-components";
+import tw from "twin.macro";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
+import "swiper/swiper-bundle.min.css";
 
-const settings: Settings = {
-  dots: true,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  arrows: false,
-  pauseOnHover: false,
-};
-
-const customSliderStyles: CSSProp = {
-  ".slick-slide, .slick-slide img": {
-    maxHeight: "65vh",
-  },
-  // Add some padding on bottom to avoid hiding overflowing img shadow
-  ".slick-list": {
-    boxSizing: "initial",
-    paddingBottom: "2rem",
-  },
-};
+SwiperCore.use([Autoplay, Pagination]);
 
 const ProjectShowcase: React.FC<{ images: string[] }> = ({ images }) => {
   return (
-    <div css={customSliderStyles}>
-      <Slider tw="mb-16" {...settings}>
-        {images.map(image => (
+    <Swiper
+      spaceBetween={50}
+      loop
+      autoplay
+      pagination={{ clickable: true }}
+      slidesPerView="auto"
+      centeredSlides
+      grabCursor
+      tw="mb-16 pb-16 pt-4"
+      css={{ ".swiper-wrapper": tw`items-center` }}
+    >
+      {images.map(image => (
+        <SwiperSlide style={{ width: "fit-content" }} key={image}>
           <img
-            tw="block! w-auto! mx-auto rounded shadow-xl object-contain lg:rounded-2xl"
+            css={{ maxHeight: "clamp(400px, 50vh, 600px)", maxWidth: "100vw" }}
+            tw="rounded-2xl shadow-xl object-contain"
             src={image}
             alt={`Project Showcase Image ${image.split("/").slice(-1)}`}
-            key={image}
           />
-        ))}
-      </Slider>
-    </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
